3482095819df9b0b49ef399a7345f8885304c465
alishaik0428 <shaikali4452@gmail.com>
2023-03-10 15:33:28 +0530

    initial commit

---
+++ src/main/java/com/restapiuser/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.restapiuser.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.restapiuser.model.UserEntity;
+import com.restapiuser.repository.UserRepository;
+
+@RestController
+@RequestMapping("/top")
+public class UserController {
+	@Autowired
+	UserRepository userepo;
+	
+	@RequestMapping("/start")
+	public String start()
+	{
+		return "You are in Main User Page";
+	}
+	
+	@GetMapping("/get")
+	public List<UserEntity> getallus()
+	{
+		return userepo.findAll();
+	}
+	
+	 @PostMapping("/post")
+	    public String createUser(@RequestBody UserEntity user) {
+	       userepo.save(user);
+	       return "User Is Created";
+	    }
+
+}
